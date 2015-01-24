6219e8192eaa256d623f00f9f9491fb93c42b09d
lazyparser <lazyparser@gmail.com>
2015-01-24 23:15:08 +0800

    C++ 0035.cpp

---
+++ 0035.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int searchInsert(int A[], int n, int target) {
+        int i = 0;
+        while (i < n && A[i] < target)
+            ++i;
+        return i;
+    }
+};
