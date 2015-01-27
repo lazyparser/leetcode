b22813aa1c8a8a04e7aaaf68750d1d3d70f80e27
lazyparser <lazyparser@gmail.com>
2015-01-27 22:26:49 +0800

    C++ 0026

---
+++ 0026.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        int len = 0;
+        if (n < 2) return n;
+        for (int i = 1; i < n; i++) {
+            if (A[len] != A[i])
+                A[++len] = A[i];
+        }
+        return len + 1;
+    }
+};
