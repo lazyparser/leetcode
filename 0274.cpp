af6a57aca98516930ea103342e2e0cb9f749ddf0
lazyparser <lazyparser@gmail.com>
2015-09-14 10:23:49 +0800

    C++ 0274

---
+++ 0274.cpp
@@ -0,0 +1,13 @@
+#274 H-Index
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int h = 0;
+        if (citations.size() == 0) return 0;
+        std::sort(citations.begin(), citations.end());
+        std::reverse(citations.begin(), citations.end());
+        while (h < citations.size() && citations[h] >= h+1)
+            h++;
+        return h;
+    }
+};
