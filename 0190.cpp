6f830de1aab8b3aece3f5b2aefde84629366a385
lazyparser <lazyparser@gmail.com>
2015-09-28 08:59:08 +0800

    C++ 0190

---
+++ 0190.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t r = 0;
+        for (int i = 0; i < 32; i++) {
+            r = (r << 1) | (n & 0x1);
+            n >>= 1;
+        }
+        return r;
+    }
+};
