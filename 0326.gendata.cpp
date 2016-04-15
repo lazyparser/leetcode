7a8866b8cf122c017c244aec11fb37a923bedde5
Wei Wu <lazyparser@gmail.com>
2016-04-15 14:18:20 +0800

    326. Power of Three

---
+++ 0326.gendata.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int powerof3 = 1;
+    while (powerof3 > 0) {
+        powerof3 *= 3;
+        cout << powerof3 << endl;
+    }
+}
