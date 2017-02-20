3f5be89c7ef977f345448055cd42ad0da3c98e97
Wei Wu <lazyparser@gmail.com>
2017-02-20 23:32:36 +0800

    0476.NumberComplement

---
+++ 0476.NumberComplement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 1;
+        while (num & (~mask))
+            mask = (mask<<1) | 1;
+        return (~num) & mask;
+    }
+};
+
