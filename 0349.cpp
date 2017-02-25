4843c8a5e48eacd108b98e85935bce18f29ccf57
Wei Wu <lazyparser@gmail.com>
2017-02-25 22:20:07 +0800

    0349

---
+++ 0349.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
+            if (nums1[i] == nums2[j]) {
+                if (result.size() == 0 || result.back() != nums1[i])
+                    result.push_back(nums1[i]);
+                ++i;
+                ++j;
+            } else if (nums1[i] < nums2[j]) {
+                ++i;
+            } else {
+                ++j;
+            }
+        }
+        return result;
+    }
+};
+
